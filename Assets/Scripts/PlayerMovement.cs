48858fca4c7856dfad2cc3366cacc353ff85f3de
hyojoon Lim <hyojoon42@naver.com>
2025-05-28 22:16:12 +0900

    feature : Player Move 추가

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector3 m_Movement;                                 //이동
+    Quaternion m_Rotation = Quaternion.identity;        //회전 생성
+    Animator m_Animator;                                //캐릭터 애니메이터
+    Rigidbody m_Rigidbody;
+
+    public float turnSpeed = 20f;     //캐릭터 회전 속도
+
+
+    //----------- 초기화 ------------
     void Start()
     {
-        
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement.Normalize();     //대각선 이동 가속화 방지
+
+        //플레이어 입력 감지
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+
+        //isWalking 이 true이면 Walking Animation Start
+        m_Animator.SetBool("IsWalking", isWalking);
+
+        //캐릭터 회전 (turnSpeed만큼 회전 속도 제어)
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+
+        //desireForward 방향으로 회전
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    //----------------- 이동 & 회전 개별 적용 ----------------
+    private void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);   //루트모션으로 인한 프레임당 이동량
+        m_Rigidbody.MoveRotation(m_Rotation);
     }
 }
