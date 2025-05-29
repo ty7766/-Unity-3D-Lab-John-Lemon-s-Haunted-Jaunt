e11afb7c4adac5609a1a707d00e354d76ff206c6
hyojoon Lim <hyojoon42@naver.com>
2025-05-29 20:44:25 +0900

    faeture : GameOver 기능 추가

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -9,18 +9,27 @@ public class GameEnding : MonoBehaviour
 
     public GameObject player;                                //플레이어
     public CanvasGroup exitBackgroundImageCanvasGroup;      //Clear Image
+    public CanvasGroup caughtBackgroundImageCanvasGroup;    //Game Over Image
    
 
     bool m_IsPlayerAtExit;      //플레이어가 Exit에 도달하였는지
+    bool m_IsPlayerCaught;      //플레이어가 잡혔는지
+
     float m_Timer;              //페이드 시간 기록
 
+    //UI 보여주기
     private void Update()
     {
         if (m_IsPlayerAtExit)
         {
-            EndLevel();
+            EndLevel(exitBackgroundImageCanvasGroup);
+        }
+        else if (m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup);
         }
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
@@ -29,11 +38,11 @@ public class GameEnding : MonoBehaviour
         }
     }
 
-    //UI Fade In
-    void EndLevel()
+    //플레이어의 게임 클리어 및 게임 오버 이미지 보여주기
+    void EndLevel(CanvasGroup imageCanvasGroup)
     {
         m_Timer += Time.deltaTime;
-        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;    //Fade In
 
         if (m_Timer > fadeDuration + displayImageDuration)
         {
